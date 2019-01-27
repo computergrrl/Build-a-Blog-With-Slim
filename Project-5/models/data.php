c1e2756e9203ac66ed86c71f646b0dc7a86bc4ef
computergrrl <44240149+computergrrl@users.noreply.github.com>
2019-01-26 19:34:04 -0800

    Comments display on detail page

--- Project-5/models/data.php
+++ Project-5/models/data.php
@@ -41,16 +41,38 @@ class Data
 
   }
 
-  public function getComments()
+  public function getComments($id = null)
   {
 
-    $sql = $this->blogdb->prepare("SELECT * FROM comments");
+    $sql = $this->blogdb->prepare("SELECT * FROM comments WHERE BlogID = ?");
+    $sql->bindValue(1, $id, PDO::PARAM_INT);
     $sql->execute();
     $results = $sql->fetchAll(PDO::FETCH_ASSOC);
     return $results;
 
+
+
   }
 
+  public function displayComments($id = null)
+  {
 
+    foreach($this->getComments($id) as $comment) {
+    echo '<div class="comment">';
+    echo "<strong>" . $comment['name'] . "</strong>";
+    echo '<time datetime="' .$comment['date'] . '">' .$comment['date'] . "</time>";
+    echo '<p>' . $comment['comment'] . '</p></div>';
+
+    }
+    return true;
+  }
+
+  // public function displayDate($id)
+  // {
+  //
+  //   $getdate = $getdata[$id]['Date'];
+  //   $date = date("F d, Y", strtotime($getdate));
+  //
+  // }
 
 }
