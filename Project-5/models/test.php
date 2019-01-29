8752b38e167cbfeeb6d2a135190418a73e5973a2
computergrrl <44240149+computergrrl@users.noreply.github.com>
2019-01-28 20:41:15 -0800

    redefined $args variables.  CSS now broken :-(

--- Project-5/models/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('../models/data.php');
-
-$blogdata = "../models/database.db";
-
-$thedata = new Data($blogdata);
-
-var_dump($thedata);
--- Project-5/src/routes.php
+++ Project-5/src/routes.php
@@ -6,34 +6,34 @@ use Slim\Http\Response;
 // Routes
 
 
-$app->get('/test', function (Request $request, Response $response, array $args) {
+// $app->get('/test', function (Request $request, Response $response, array $args) {
+//
+//     $args['db'] = $this->db;
+//     $args['data'] = $this->data;
+//     $args['blog'] = $this->data->getData();
+//     // $args['id'] = $_GET['q'] ;
+//     $this->logger->info("Amber was here '/detail' route");
+//
+//
+//
+//     return $this->renderer->render($response, 'test.spark', $args);
+//
+// });
+/**************DETAIL PAGE *****************************************/
+$app->map(['GET', 'POST'], '/detail/{q}', function (Request $request, Response $response, array $args) {
 
-    $args['db'] = $this->db;
-    $args['data'] = $this->data;
-    $args['blog'] = $this->data->getData();
-    $args['id'] = $_GET['q'] ;
-    $this->logger->info("Amber was here '/detail' route");
-
-
-
-    return $this->renderer->render($response, 'test.spark', $args);
-
-});
-
-$app->map(['GET', 'POST'], '/detail', function (Request $request, Response $response, array $args) {
 
     $args['db'] = $this->db;
     $args['data'] = $this->data;
     $args['comments'] = $this->comments;
     $args['blog'] = $this->data->getData();
-    $args['id'] = $_GET['q'] ;
     $this->logger->info("Amber was here '/detail' route");
 
     if($request->getMethod() == 'POST') {
 
     $args = array_merge($args, $request->getParsedBody());
 
-      $id = $args['id'];
+      $id = $args['q'];
       $name = $args['name'];
 
       //if no name is entered, then the name "Anonymous" will be used
@@ -53,6 +53,8 @@ $app->map(['GET', 'POST'], '/detail', function (Request $request, Response $resp
     return $this->renderer->render($response, 'detail.spark', $args);
 
 });
+
+/************ NEW ENTRY PAGE ***************************************/
 
 $app->map(['GET', 'POST'], '/new', function (Request $request, Response $response, array $args) {
 
@@ -88,14 +90,14 @@ $app->map(['GET', 'POST'], '/new', function (Request $request, Response $respons
 
 });
 
+/*******************   EDIT PAGE  ***********************************/
 
-$app->map(['GET', 'POST'], '/edit', function (Request $request, Response $response, array $args) {
+$app->map(['GET', 'POST'], '/edit/{q}', function (Request $request, Response $response, array $args) {
 
 
     $args['db'] = $this->db;
     $args['data'] = $this->data;
     $args['blog'] = $this->data->getData();
-    $args['id'] = $_GET['q'];
     $this->logger->info("Amber was here '/edit' route");
 
 
@@ -106,36 +108,16 @@ $app->map(['GET', 'POST'], '/edit', function (Request $request, Response $respon
 
           $title = $args['title'];
           $entry = $args['entry'] ;
-          $id = $args['id'];
+          $id = $args['q'];
           if(!empty($args['title']) && !empty($args['entry'])) {
            $this->data->editData($title, $entry, $id);
 
           }
-
-          else {echo "These are empty!";}
-
-    }
-
+        }
 
     return $this->renderer->render($response, 'edit.spark', $args);
 
 });
-
-
-$app->get('/pop', function (Request $request, Response $response, array $args) {
-
-
-    $args['db'] = $this->db;
-    $args['data'] = $this->data;
-    $args['blog'] = $this->data->getData();
-    $this->logger->info("Amber was here '/' route");
-
-
-
-    return $this->renderer->render($response, 'population_arrays.php', $args);
-
-});
-
 
 $app->get('/', function (Request $request, Response $response, array $args) {
 
@@ -148,5 +130,26 @@ $app->get('/', function (Request $request, Response $response, array $args) {
 
 
     return $this->renderer->render($response, 'index.spark', $args);
+
+});
+
+
+
+
+/*********************************************************************
+/********** JUST FOR FUN - DATABASE POPULATION PAGE ******************
+**********************************************************************/
+
+$app->get('/pop', function (Request $request, Response $response, array $args) {
+
+
+    $args['db'] = $this->db;
+    $args['data'] = $this->data;
+    $args['blog'] = $this->data->getData();
+    $this->logger->info("Amber was here '/' route");
+
+
+
+    return $this->renderer->render($response, 'population_arrays.php', $args);
 
 });
